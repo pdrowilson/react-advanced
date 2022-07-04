7c1190082234ede51cd817cd9f32493b3f3e5a03
pdrowilson <pdro.wilson@gmail.com>
2022-07-04 15:25:51 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import doggy from './assets/dog.jpg';
+
+function App() {
+  return (
+    <div>
+      <h1>React Advanced</h1>
+      <img src={doggy} alt="" />
+    </div>
+  )
+}
+
+export default App
